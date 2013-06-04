26d95be17e0609c3003379aa64ad3c3d3aa5e47e
pprun <zhigang.pi@hadventure.com>
2013-06-04 14:20:37 +0800

    init checkin

---
+++ src/git/demo/GitDemo.java
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (c) 2013 北京海达网畅信息技术开发有限公司（www.hadventure.com）. All rights reserved.
+ * 
+ *  HADVENTURE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 
+ */
+
+package git.demo;
+
+/**
+ *
+ * @author <a href="mailto:zhigang.pi@hadventure.com">pprun</a>
+ */
+public class GitDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
